59b402f8b0ca42a7105c3cfb41d3d1d6599b4a14
Sosnov Max <crazymax101@gmail.com>
2014-10-08 20:10:50 +0700

    New task. waste 160 seconds of my life
    Algorithm process Nth fibonachi number

---
+++ taskExamples/fibonachiMember/fibonach.py
@@ -0,0 +1,30 @@
+class fibonachiNumbers:
+	def __init__(self, number):
+		self.a = 0
+		self.b = 1
+		for i in range (0, number):
+			self.procNext()
+	def procNext(self):	
+		c = self.a + self.b
+		self.a = self.b
+		self.b = c
+	def get(self):
+		return self.b
+
+
+import sys
+sys.stdout = open('log', 'w')
+
+import time
+
+start_time = time.time()
+
+number = 1000000
+fnumber = fibonachiNumbers(number).get()
+
+elapsed_time = time.time() - start_time
+
+print('{0} number of fibonachi is {1}'.format(number, fnumber))
+print('elapsed time {0}'.format(elapsed_time))
+
+sys.stdout.close()
