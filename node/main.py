ba97ae8e838360c872a42fab247ab17dc0b4753d
Sosnov Max <crazymax101@gmail.com>
2014-10-16 19:23:59 +0700

    Remove path to python in config.ini #28

--- node/main.py
+++ node/main.py
@@ -8,6 +8,6 @@ config.read("config.ini")
 
 ip = config['SERVER']['IP']
 port = int(config['SERVER']['PORT'])
-pythonPath = config['PYTHON']['PATH']
+pythonPath = sys.executable
 
 node.node(ip, port, pythonPath)
